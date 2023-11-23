03db858d3c9a24bef051241c72d0028675fee4d6
mynhardtc <21283708+mynhardtc@users.noreply.github.com>
2023-11-23 20:54:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Create an application to estimate the clothing size based on height, weight and age.
+# create a separate function for doing the estimation
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
